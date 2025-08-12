f8e45b81411d7b099325d3cff3631272724af5a0
dachka123 <dachijavaxishvili@gmail.com>
2025-08-12 13:55:24 +0400

    status bar fixed

--- app/src/main/java/com/example/moviesapi/presentation/UI/MoviesListCard.kt
+++ app/src/main/java/com/example/moviesapi/presentation/UI/MoviesListCard.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
@@ -27,6 +28,7 @@ fun MovieListCard(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFF121212))
+            .statusBarsPadding()
     ) {
         Text(
             text = stringResource(R.string.movies),
@@ -34,7 +36,7 @@ fun MovieListCard(
             fontSize = 32.sp,
             fontWeight = FontWeight.Bold,
             modifier = Modifier
-                .padding(start = Dimens.spacing24,top = Dimens.spacing34)
+                .padding(start = Dimens.spacing24)
         )
 
         LazyColumn(
