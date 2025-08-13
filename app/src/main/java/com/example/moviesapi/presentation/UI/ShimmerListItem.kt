2ffc334d81182a438c62efb4e1152949966e55d1
dachka123 <dachijavaxishvili@gmail.com>
2025-08-13 12:02:39 +0400

    error screen added

--- app/src/main/java/com/example/moviesapi/presentation/UI/ShimmerListItem.kt
+++ app/src/main/java/com/example/moviesapi/presentation/UI/ShimmerListItem.kt
@@ -1,6 +1,8 @@
 package com.example.moviesapi.presentation.UI
 
 import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateFloat
+
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
